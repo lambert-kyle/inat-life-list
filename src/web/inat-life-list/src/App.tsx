e0650cf2a3a5f7ba080cbcdf715cf12489d58a0f
Kyle Lambert <klambert@acvauctions.com>
2025-04-28 09:55:58 -0400

    Remove api connection and health check

--- src/web/inat-life-list/src/App.tsx
+++ src/web/inat-life-list/src/App.tsx
@@ -2,7 +2,6 @@ import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HealthCheck from "./health-check/HealthCheck.tsx";
 
 function App() {
     const [count, setCount] = useState(0)
@@ -29,7 +28,6 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
-            <HealthCheck/>
         </>
     )
 }
