f580dcee7dde624ac03d91499dbe6ddd1217d9f6
Kyle Lambert <klambert@acvauctions.com>
2025-05-05 16:16:53 -0400

    zip up data before tabling

--- src/web/inat-life-list/src/components/LifeList.tsx
+++ src/web/inat-life-list/src/components/LifeList.tsx
@@ -3,12 +3,17 @@ import { useTopSpecies } from '../hooks/useTopSpecies.ts';
 import SettingsModal from '../modules/settings/SettingsModal.tsx';
 import { useSettings } from '../modules/settings/useSettings.tsx';
 import { useUserObservations } from '../modules/observations/useUserObservations.ts';
+import useUser from '../modules/settings/user/useUser.ts';
 
 export const LifeList = (): React.ReactElement => {
     const [settingsOpen, setSettingsOpen] = useState(false);
     const { latitude, limit, longitude, radiusKm, userId } = useSettings();
 
-    const { data, error, isLoading } = useTopSpecies({
+    const {
+        data: topSpecies,
+        error,
+        isLoading,
+    } = useTopSpecies({
         lat: latitude,
         lng: longitude,
         radius: radiusKm,
@@ -20,6 +25,20 @@ export const LifeList = (): React.ReactElement => {
     React.useEffect(() => {
         console.log({ latitude, limit, longitude, radiusKm, userId });
     }, [latitude, limit, longitude, radiusKm, userId]);
+    const { data: user } = useUser(userId);
+
+    const results = React.useMemo(
+        () =>
+            topSpecies?.map((species) => ({
+                id: species.id,
+                scientificName: species.name,
+                commonName: species.preferred_common_name,
+                observationsCount: species.observations_count,
+                photoUrl: species.default_photo?.square_url,
+                seen: userTaxa?.has(species.id),
+            })),
+        [topSpecies, userTaxa]
+    );
 
     return (
         <div>
@@ -41,12 +60,13 @@ export const LifeList = (): React.ReactElement => {
             </div>
             <span>
                 Showing top {limit} species within {radiusKm} km of (
-                {latitude?.toFixed(2)}, {longitude?.toFixed(2)})
+                {latitude?.toFixed(2)}, {longitude?.toFixed(2)}) and whether{' '}
+                {user?.login} has observed them
             </span>
 
             {isLoading && <p>Loading...</p>}
             {error && <p>Error: {error.message}</p>}
-            {data && (
+            {topSpecies && (
                 <table>
                     <thead>
                         <tr>
@@ -58,40 +78,33 @@ export const LifeList = (): React.ReactElement => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((species) => {
-                            const seen = userTaxa?.has(species.id);
-
-                            return (
-                                <tr key={species.id}>
-                                    <td>
-                                        {species.default_photo?.square_url ? (
-                                            <img
-                                                src={
-                                                    species.default_photo
-                                                        .square_url
-                                                }
-                                                alt={
-                                                    species.preferred_common_name ||
-                                                    species.name
-                                                }
-                                                width="50"
-                                                height="50"
-                                            />
-                                        ) : (
-                                            'N/A'
-                                        )}
-                                    </td>
-                                    <td>
-                                        <i>{species.name}</i>
-                                    </td>
-                                    <td>
-                                        {species.preferred_common_name || 'N/A'}
-                                    </td>
-                                    <td>{species.observations_count}</td>
-                                    <td>{seen ? '✅ Seen' : '❌ Not Seen'}</td>
-                                </tr>
-                            );
-                        })}
+                        {results?.map((species) => (
+                            <tr key={species.id}>
+                                <td>
+                                    {species.photoUrl ? (
+                                        <img
+                                            src={species.photoUrl}
+                                            alt={
+                                                species.commonName ||
+                                                species.scientificName
+                                            }
+                                            width="50"
+                                            height="50"
+                                        />
+                                    ) : (
+                                        'N/A'
+                                    )}
+                                </td>
+                                <td>
+                                    <i>{species.scientificName}</i>
+                                </td>
+                                <td>{species.commonName || 'N/A'}</td>
+                                <td>{species.observationsCount}</td>
+                                <td>
+                                    {species.seen ? '✅ Seen' : '❌ Not Seen'}
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             )}
